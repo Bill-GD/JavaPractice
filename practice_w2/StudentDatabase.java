f8e79d3c092508de5ee247a6a6dd030a8c4c30e2
Bill-GD <geometrydashbill@gmail.com>
2023-04-05 17:54:45 +0700

    Week 2-2

--- practice_w2/StudentDatabase.java
+++ practice_w2/StudentDatabase.java
@@ -16,32 +16,44 @@ public class StudentDatabase {
                     "create table if not exists SinhVien (MaSV int, TenSV varchar(50), Khoa varchar(50));");
             statement.executeUpdate("delete from SinhVien");
 
-            int maxRecordCount = 20;
+            int maxRecordCount = 500;
             for (int i = 0; i < maxRecordCount; i++) {
-                String name = Character.toString((char) (i + 65));
-                String major = (i % 3 == 0 || i % 7 == 0) ? "CNTT" : "Biology";
+                if (i + 33 >= 127)
+                    break;
+                if (i + 33 == 39 || i + 33 == 92) {
+                    continue;
+                }
+                String name = Character.toString((char) (i + 33));
+                String major = (i % 3 == 0) ? "Biology"
+                        : (i % 7 == 0) ? "Chemistry"
+                        : (i % 5 == 0) ? "Engineer"
+                        : (i % 2 == 0) ? "Tour" : "CNTT";
 
-                String insertStatement = "insert into SinhVien values (" + (i + 1) + ", '" + name + "', '" + major + "')";
+                String insertStatement = "insert into SinhVien values (" + (i + 1) + ", '" + name + "', '" + major
+                        + "')";
                 // use executeUpdate for all update statements
                 statement.executeUpdate(insertStatement);
                 System.out.println("Inserted (" + (i + 1) + ", '" + name + "', '" + major + "')");
             }
 
-            int row = statement.executeUpdate("delete from SinhVien where MaSV=4");
-            System.out.println("Deleted " + row + " record(s)");
+            int rowDeleted = statement.executeUpdate("delete from SinhVien where MaSV=4");
+            System.out.println("Deleted " + rowDeleted + " record(s)");
 
             String queryString = "select * from sinhvien where Khoa='CNTT';";
             System.out.println("Query: " + queryString);
             // use executeQuery for queries
             ResultSet resultSet = statement.executeQuery(queryString);
 
+            int rowCount = 0;
             while (resultSet.next()) {
                 int studentID = resultSet.getInt("MaSV");
                 String studentName = resultSet.getString("TenSV");
                 String major = resultSet.getString("Khoa");
 
                 System.out.println(studentID + ", " + studentName + ", " + major);
+                rowCount++;
             }
+            System.out.println("Row count: " + rowCount);
         } catch (Exception e) {
             System.out.println(e.toString());
         }
