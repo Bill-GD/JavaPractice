eee08136ac96ea700824136be2506f9fff1fa727
Bill-GD <geometrydashbill@gmail.com>
2023-04-12 17:53:33 +0700

    Week 3-2

--- practice_w3/TestSocketClient.java
+++ practice_w3/TestSocketClient.java
@@ -1,34 +1,43 @@
 package practice_w3;
 
+// imports to use Socket, Reader, Writer
 import java.net.*;
 import java.io.*;
 
 public class TestSocketClient {
     public static void main(String[] args) {
+        // specifies port number
         int port = 1234;
         try {
+            // creates a socket that connects to the specified port at localhost
             Socket socket = new Socket(InetAddress.getByName("localhost"), port);
 
+            // notifies user if the socket is connected
             if (socket.isConnected())
                 System.out.println("Connected using port " + port);
 
+            // creates a reader to read input from console (user)
             BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 
+            // saves the inputs
             System.out.print("Enter first number: ");
-            double firstNum = Double.parseDouble(br.readLine());
+            String firstNum = br.readLine();
             System.out.print("Enter Second number: ");
-            double secondNum = Double.parseDouble(br.readLine());
+            String secondNum = br.readLine();
 
+            // creates a writer to write to the socket
             PrintWriter pw = new PrintWriter(new OutputStreamWriter(socket.getOutputStream()), true);
 
+            // writes to socket
             pw.println(firstNum);
             pw.println(secondNum);
 
+            // reader for reading from socket
             br = new BufferedReader(new InputStreamReader(socket.getInputStream()));
             for (int i = 0; i < 4; i++)
-                System.out.println(br.readLine());
+                System.out.println(br.readLine()); // output the received messages from socket
 
-        } catch (Exception e) {
+        } catch (Exception e) { // handles exceptions of Socket()
             System.out.println(e.toString());
         }
     }
--- practice_w3/TestSocketServer.java
+++ practice_w3/TestSocketServer.java
@@ -1,38 +1,49 @@
 package practice_w3;
 
+// imports to use ServerSocket, Reader, Writer
 import java.net.*;
 import java.io.*;
 
 public class TestSocketServer {
     public static void main(String[] args) {
+        // specifies port number
         int port = 1234;
         try {
+            // create a server socket at the specified port
             ServerSocket serverSocket = new ServerSocket(port);
 
+            // loop to continuously listen
             while (true) {
                 System.out.println("Listening at port " + port + "...");
+                // waits for a connection then accepts it
                 Socket socket = serverSocket.accept();
 
+                // creates reader to read from socket
                 BufferedReader br = new BufferedReader(new InputStreamReader(socket.getInputStream()));
 
+                // creates writer to write to socket
                 PrintWriter pw = new PrintWriter(new OutputStreamWriter(socket.getOutputStream()), true);
 
+                // stores the inputs from socket
                 double firstNum = Double.parseDouble(br.readLine());
                 double secondNum = Double.parseDouble(br.readLine());
 
-                System.out.println("Received from " + socket.getInetAddress().getHostAddress());
+                // notifies (server-side) that the server has received inputs from port
+                System.out.println("Received from " + socket.getInetAddress().getHostAddress() + '\n');
 
+                // calculations
                 double sum = firstNum + secondNum;
                 double sub = firstNum - secondNum;
                 double mul = firstNum * secondNum;
                 double div = firstNum / secondNum;
 
+                // sends the results back
                 pw.println("Sum: " + sum);
                 pw.println("Sub: " + sub);
                 pw.println("Mul: " + mul);
                 pw.println("Div: " + div);
             }
-        } catch (Exception e) {
+        } catch (Exception e) { // handles exception of ServerSocket()
             System.out.println(e.toString());
         }
     }
